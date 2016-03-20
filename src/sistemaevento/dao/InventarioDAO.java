4c5bbe0424b2194f9fd06e3c27fd68aa0450c19a
Xavier <Xavier@Angely>
2016-03-19 23:37:10 -0500

    Pantalla de Transporte: buscar

--- src/sistemaevento/dao/InventarioDAO.java
+++ src/sistemaevento/dao/InventarioDAO.java
@@ -4,11 +4,14 @@ import java.sql.SQLException;
 import java.util.List;
 
 import sistemaevento.dtos.ArticuloDTO;
+import sistemaevento.dtos.TransporteDTO;
 
 public interface InventarioDAO {
 
 	public List<ArticuloDTO> buscarArticulo(ArticuloDTO articuloFilter) throws SQLException;
 
 	public ArticuloDTO obtenerArticuloPorId(Long id) throws SQLException;
+	
+	public List<TransporteDTO> buscarTransporte(TransporteDTO transporteFilter) throws SQLException;
 
 }
